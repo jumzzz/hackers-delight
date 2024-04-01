86bbb6379103365eea8f7db69cfa216e0fbfdba5
jumzzz <jummel19@gmail.com>
2024-04-01 16:11:49 +0800

    yes

--- ch-02/scratch-pad/right-most-bits/right_most.c
+++ ch-02/scratch-pad/right-most-bits/right_most.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 #include "bit_utils.h"
 
-// Use the following formula to turn off the rightmost 1-bit in a word, producing 
-// 0 if none (e.g., 01011000  01010000):
-// Formula:
-// x & (x - 1)
 
 typedef unsigned int uint32;
 typedef void const* const const_void_ptr;
@@ -27,25 +23,56 @@ void print_ops_bits(
     printf("------------------------------------\n\n");
 }
 
-void remove_rightmost(uint32 lhs) {
+/*
+   Use the following formula to turn off the rightmost 1-bit in a word, producing 
+   0 if none (e.g., 01011000  01010000):
+   
+   Formula:
+        x & (x - 1)
+*/
+uint32 off_rightmost(uint32 lhs) {
     uint32 rhs = lhs - 1;
     uint32 res = lhs & rhs;
     printf("------------------------------------\n");
-    printf("remove_right_most(%u)\n", lhs);
+    printf("off_rightmost(%u)\n", lhs);
     print_ops_bits(" & ", &lhs, &rhs, &res, sizeof(lhs), sizeof(rhs), sizeof(res));  
+    return res;
 }
 
+/*
+   Use the following formula to turn on the rightmost 1-bit in a word, producing 
+   0 if none (e.g., 10100111  10101111): 
+   
+   Formula:
+        x | (x + 1)
+*/
+uint32 on_rightmost(uint32 lhs) {
+    uint32 rhs = lhs + 1;
+    uint32 res = lhs | rhs;
+    printf("------------------------------------\n");
+    printf("on_rightmost(%u)\n", lhs);
+    print_ops_bits(" | ", &lhs, &rhs, &res, sizeof(lhs), sizeof(rhs), sizeof(res));  
+    return res;
+}
 
 int main(int argc, char* argv[]) {
-    uint32 n0 = 256 + 128 + 64 + 32 + 16 + 8 + 4 + 2 + 1;
-    uint32 n1 = 256; 
-    uint32 n2 = 256 + 64 +  16 +  4 +  1;
-    uint32 n3 = 128 + 1;
+    uint32 off_n0 = 128 + 64 + 32 + 16 + 8 + 4 + 2 + 1;
     
-    remove_rightmost(n0);
-    remove_rightmost(n1);
-    remove_rightmost(n2);
-    remove_rightmost(n3);
+    uint32 off_n1 = off_rightmost(off_n0);
+    uint32 off_n2 = off_rightmost(off_n1);
+    uint32 off_n3 = off_rightmost(off_n2);
+    uint32 off_n4 = off_rightmost(off_n3);
+    uint32 off_n5 = off_rightmost(off_n4);
+    uint32 off_n6 = off_rightmost(off_n5);
+    uint32 off_n7 = off_rightmost(off_n6);
+    
+    uint32 on_n1 = on_rightmost(off_n7);
+    uint32 on_n2 = on_rightmost(off_n1);
+    uint32 on_n3 = on_rightmost(off_n2);
+    uint32 on_n4 = on_rightmost(off_n3);
+    uint32 on_n5 = on_rightmost(off_n4);
+    uint32 on_n6 = on_rightmost(off_n5);
+    uint32 on_n7 = on_rightmost(off_n6);
 
     return 0;
 }
