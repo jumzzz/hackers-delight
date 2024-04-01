469662e61def154f1750761d997a29b0d1d4606e
jumzzz <jummel19@gmail.com>
2024-04-01 16:49:35 +0800

    yes

--- ch-02/scratch-pad/right-most-bits/right_most.c
+++ ch-02/scratch-pad/right-most-bits/right_most.c
@@ -55,6 +55,22 @@ uint32 on_rightmost(uint32 lhs) {
     return res;
 }
 
+/*
+   Use the following formula to turn off trailing 1's in a word, producing 
+   x if none (e.g., 10100111  10100000): 
+   
+   Formula:
+        x & (x + 1)
+*/
+uint32 off_rightmost_trailing_ones(uint32 lhs) {
+    uint32 rhs = lhs + 1;
+    uint32 res = lhs & rhs;
+    printf("------------------------------------\n");
+    printf("off_trailing_ones(%u)\n", lhs);
+    print_ops_bits(" & ", &lhs, &rhs, &res, sizeof(lhs), sizeof(rhs), sizeof(res));  
+    return res;
+}
+
 int main(int argc, char* argv[]) {
     uint32 off_n0 = 8 + 4 + 2 + 1;
     
@@ -74,6 +90,14 @@ int main(int argc, char* argv[]) {
     uint32 on_n1 = on_rightmost(off_n3);
     uint32 on_n2 = on_rightmost(on_n1);
     uint32 on_n3 = on_rightmost(on_n2);
+
+    uint32 on_off_trails = 2048 + 1024 + 512 + 256  + 8 + 4 + 2 + 1;
+    
+    printf("\n");
+    printf("------------------------------------\n");
+    printf("Turning Off Right Most Trailing Ones: \n");
+    printf("------------------------------------\n");
+    uint32 off_trails = off_rightmost_trailing_ones(on_off_trails);
 
     return 0;
 }
