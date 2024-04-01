e3b8606244bb4e77591300ab150d6ef959f3a715
jumzzz <jummel19@gmail.com>
2024-04-01 15:51:28 +0800

    yes bit manipulation

---
+++ ch-02/scratch-pad/right-most-bits/bit_utils.h
@@ -0,0 +1,36 @@
+#ifndef BIT_UTILS_H
+#define BIT_UTILS_H
+
+typedef unsigned char uchar;
+
+void print_bits(
+        char* start_msg, 
+        void const* const ptr,
+        size_t const size ) {
+    uchar *b = (unsigned char*) ptr;
+    printf("%s", start_msg);
+    
+    for (int i = size - 1; i >= 0; i--) {
+        uchar b7 = (b[i] >> 7) & 1;
+        uchar b6 = (b[i] >> 6) & 1;
+        uchar b5 = (b[i] >> 5) & 1;
+        uchar b4 = (b[i] >> 4) & 1;
+        uchar b3 = (b[i] >> 3) & 1;
+        uchar b2 = (b[i] >> 2) & 1;
+        uchar b1 = (b[i] >> 1) & 1;
+        uchar b0 = (b[i] >> 0) & 1;
+        
+        printf("%u", b7);
+        printf("%u", b6);
+        printf("%u", b5);
+        printf("%u", b4);
+        printf("%u", b3);
+        printf("%u", b2);
+        printf("%u", b1);
+        printf("%u", b0);
+    }
+    puts("");
+}
+
+#endif // EXAMPLE_H
+
---
+++ ch-02/scratch-pad/right-most-bits/right_most.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include "bit_utils.h"
+
+// Use the following formula to turn off the rightmost 1-bit in a word, producing 
+// 0 if none (e.g., 01011000  01010000):
+// Formula:
+// x & (x - 1)
+
+typedef unsigned int uint32;
+typedef void const* const const_void_ptr;
+typedef char* char_ptr;
+typedef size_t const const_size_t;
+
+void print_ops_bits(
+        const_void_ptr lhs_ptr, 
+        const_void_ptr rhs_ptr,
+        const_void_ptr res_ptr,
+        const_size_t lhs_size,
+        const_size_t rhs_size,
+        const_size_t res_size) {
+
+    printf("------------------------------------\n");
+    print_bits("   ", lhs_ptr, lhs_size);
+    print_bits(" & ", rhs_ptr, rhs_size); 
+    print_bits(" = ", res_ptr, res_size);
+    printf("------------------------------------\n\n");
+}
+
+void remove_rightmost(uint32 lhs) {
+    uint32 rhs = lhs - 1;
+    uint32 res = lhs & rhs;
+    
+    printf("------------------------------------\n");
+    printf("remove_right_most(%u)\n", lhs);
+    print_ops_bits(&lhs, &rhs, &res, sizeof(lhs), sizeof(rhs), sizeof(res));  
+}
+
+
+int main(int argc, char* argv[]) {
+    uint32 n0 = 256 + 128 + 64 + 32 + 16 + 8 + 4 + 2 + 1;
+    uint32 n1 = 256; 
+    uint32 n2 = 256 + 64 +  16 +  4 +  1;
+    uint32 n3 = 128 + 1;
+    
+    remove_rightmost(n0);
+    remove_rightmost(n1);
+    remove_rightmost(n2);
+    remove_rightmost(n3);
+
+    return 0;
+}
