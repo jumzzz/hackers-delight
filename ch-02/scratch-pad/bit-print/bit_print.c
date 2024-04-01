12f42d3e13d6768002f2948b6a7146f1359814b1
jumzzz <jummel19@gmail.com>
2024-04-01 15:22:10 +0800

    yes

--- ch-02/scratch-pad/bit-print/bit_print.c
+++ ch-02/scratch-pad/bit-print/bit_print.c
@@ -4,9 +4,13 @@
 
 
 int main() {
-    int number = 256 + 128 + 64 + 32 + 16 + 8 + 4 + 2 + 1;
-    print_bits("print_bits_v1 = ", &number, sizeof(number));
-    print_bits("print_bits_v2 = ", &number, sizeof(number));
+    int n0 = 256 + 128 + 64 + 32 + 16 + 8 + 4 + 2 + 1;
+    int n1 = 256 + 64 + 16 + 4 + 1;
+    int n2 = 512 + 128 + 32 + 8 + 2;
+
+    print_bits("n0 = ", &n0, sizeof(n0));
+    print_bits("n1 = ", &n1, sizeof(n1));
+    print_bits("n2 = ", &n2, sizeof(n2));
 
     return 0;
 }
